3beee15ba775152a6a116bc3dac9501d4b382229
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-18 16:27:57 +0100

    create Image upload pages

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ImageFolders from "./Components/Pages/ImageFolders";
+import Upload from "./Components/Pages/ImageUpload/Upload";
 
-ReactDOM.render(<ImageFolders />, document.getElementById("root"));
+ReactDOM.render(<Upload />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
