5dd5e1565e0e3f1f84cb2c35c4fef5d51adaf138
keith <keith@keiths-air.wireless.nuigalway.ie>
2019-06-19 16:18:30 +0100

    Commented Spinner.js

--- src/Components/Pages/ImageUpload/Spinner.js
+++ src/Components/Pages/ImageUpload/Spinner.js
@@ -1,5 +1,6 @@
 /**
 |--------------------------------------------------
 | This page will show when the user uploads files
+This will be used when the files are being upladed
 |--------------------------------------------------
 */
