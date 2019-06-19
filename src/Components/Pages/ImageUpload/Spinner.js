7f0304fffc3c661dd7f1acf5ef03a3d628e96307
KeithFarnan <k.farnan1@nuigalway.ie>
2019-06-19 16:34:22 +0100

    Removed unused code

--- src/Components/Pages/ImageUpload/Spinner.js
+++ src/Components/Pages/ImageUpload/Spinner.js
@@ -1,6 +1,5 @@
 /**
 |--------------------------------------------------
-| This page will show when the user uploads files
-This will be used when the files are being upladed
+
 |--------------------------------------------------
 */
