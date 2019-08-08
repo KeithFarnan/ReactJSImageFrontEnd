c86312d217bc89aede39b71ff432a7f3fa6724a5
Keith <k.farnan1@nuigalway.ie>
2019-08-08 16:40:10 +0100

    Updated set Auth Token file

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const setAuthToken = token => {
+// the set auth token arrow function takes the token and puts it into the headers.common for the user
+export default setAuthToken = token => {
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
   } else {
+    // if there is no token recieved the token in the header is deleted
     delete axios.defaults.headers.common['x-auth-token'];
   }
 };
-
-export default setAuthToken;
