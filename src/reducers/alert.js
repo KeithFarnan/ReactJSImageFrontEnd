fd315de6a9af1278b0970ab5ee7c73ac95c94400
Keith <k.farnan1@nuigalway.ie>
2019-08-08 16:40:52 +0100

    Updated reducers for the application

--- src/reducers/alert.js
+++ src/reducers/alert.js
@@ -1,16 +1,21 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
+// setting the initital state to empty array
 const initialState = [];
 
+// the exported function from the component which takes in the inital state array and an action as parameters
 export default function(state = initialState, action) {
+  // extracting the type and payload from the action
   const { type, payload } = action;
 
+  // creating a swithch statement for the type of action
   switch (type) {
     case SET_ALERT:
       return [...state, payload];
     case REMOVE_ALERT:
       return state.filter(alert => alert.id !== payload);
     default:
+      // returns the state if nether case is met
       return state;
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import auth from './auth';
 import profile from './profile';
 import post from './post';
 
+// this is the default entry point for the application to get to all the reducers and all the reducer methods are linked to
 export default combineReducers({
   alert,
   auth,
