0bb8250427cee1f7b7c3202daed0713363d87e13
Keith <k.farnan1@nuigalway.ie>
2019-08-09 14:07:45 +0100

    Updated the dashboard page and actions

--- src/Components/dashboard/Dashboard.js
+++ src/Components/dashboard/Dashboard.js
@@ -4,44 +4,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
-import Experience from './Experience';
-import Education from './Education';
-import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
-const Dashboard = ({
-  getCurrentProfile,
-  deleteAccount,
-  auth: { user },
-  profile: { profile, loading }
-}) => {
-  useEffect(() => {
-    getCurrentProfile();
-  }, [getCurrentProfile]);
+const Dashboard = ({ auth: { user } }) => {
+  useEffect(() => []);
 
-  return loading && profile === null ? (
+  // if the loading state is null then return the spinner else return the fragments of the code
+  return loading === null ? (
     <Spinner />
   ) : (
+    // this fragment of code executes if the loading component is not null
     <Fragment>
-      <h1 className='large text-primary'>Dashboard</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome {user && user.name}
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        {/* prints a welcome with the user name to the page */}
+        <i className="fas fa-user" /> Welcome {user && user.name}
       </p>
-      {profile !== null ? (
+
+      {user !== null ? (
         <Fragment>
           <DashboardActions />
-          <Experience experience={profile.experience} />
-          <Education education={profile.education} />
-
-          <div className='my-2'>
-            <button className='btn btn-danger' onClick={() => deleteAccount()}>
-              <i className='fas fa-user-minus' /> Delete My Account
-            </button>
-          </div>
         </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
-          <Link to='/create-profile' className='btn btn-primary my-1'>
+          <Link to="/create-profile" className="btn btn-primary my-1">
             Create Profile
           </Link>
         </Fragment>
@@ -50,19 +36,14 @@ const Dashboard = ({
   );
 };
 
+// the auth of the user is confirmed to be an object
 Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  deleteAccount: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired
 };
 
+// auth is taken out of the state and mapped to the auth property
 const mapStateToProps = state => ({
-  auth: state.auth,
-  profile: state.profile
+  auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { getCurrentProfile, deleteAccount }
-)(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
