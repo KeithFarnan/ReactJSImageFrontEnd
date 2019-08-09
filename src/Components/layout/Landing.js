9288bede18f55ce11180c5a6e6261e111611db29
Keith <k.farnan1@nuigalway.ie>
2019-08-09 12:06:55 +0100

    Updated Landing and Navbar pages

--- src/Components/layout/Landing.js
+++ src/Components/layout/Landing.js
@@ -3,25 +3,26 @@ import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// the landing page only shows when the user is not logged in so if the user is logged in the page redirects to the dashboard
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
+    return <Redirect to="/dashboard" />;
   }
-
+  // if the user is not authenticated the login form is returned
   return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
-          <p className='lead'>
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Developer Connector</h1>
+          <p className="lead">
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </p>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
             </Link>
-            <Link to='/login' className='btn btn-light'>
+            <Link to="/login" className="btn btn-light">
               Login
             </Link>
           </div>
@@ -31,10 +32,12 @@ const Landing = ({ isAuthenticated }) => {
   );
 };
 
+// the proptype of isAuthenticated to be a boolean value
 Landing.propTypes = {
   isAuthenticated: PropTypes.bool
 };
 
+// the value for isAuthenticated is retrieved from the state
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 });
--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -3,50 +3,47 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
-
+// arrow function takes the value for authenticated user or logout for the user
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  // authenticated users are shown the following links
   const authLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
+        <Link to="/images">Images</Link>
       </li>
       <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
-        <Link to='/dashboard'>
-          <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{' '}
+          <span className="hide-sm">Dashboard</span>
         </Link>
       </li>
       <li>
-        <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt' />{' '}
-          <span className='hide-sm'>Logout</span>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt" />{' '}
+          <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
   );
 
+  // links that will be displayed to non authenticated users
   const guestLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
+        <Link to="/register">Register</Link>
       </li>
       <li>
-        <Link to='/register'>Register</Link>
-      </li>
-      <li>
-        <Link to='/login'>Login</Link>
+        <Link to="/login">Login</Link>
       </li>
     </ul>
   );
 
+  // this is what is returned in both cases and the links are slotted into the spaces
   return (
-    <nav className='navbar bg-dark'>
+    <nav className="navbar bg-dark">
       <h1>
-        <Link to='/'>
-          <i className='fas fa-code' /> DevConnector
+        <Link to="/">
+          <i className="fas fa-code" /> DevConnector
         </Link>
       </h1>
       {!loading && (
@@ -56,11 +53,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
 };
 
+// the proptypes of the variables are confirmed
 Navbar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
 
+// the values of the auth for the user is recieved from the state
 const mapStateToProps = state => ({
   auth: state.auth
 });
