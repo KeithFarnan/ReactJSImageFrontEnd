7996d71f8842b7bcf538885e7eadea517211d22f
Keith <k.farnan1@nuigalway.ie>
2019-08-23 13:34:13 +0100

    Removed unuesed files and routes

--- src/Components/routes/Routes.js
+++ src/Components/routes/Routes.js
@@ -3,12 +3,10 @@ import { Route, Switch } from 'react-router-dom';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Alert from '../layout/Alert';
-import Dashboard from '../dashboard/Dashboard';
 import Events from '../events/Events';
 import Event from '../event/Event';
 import NotFound from '../layout/NotFound';
 // import PrivateRoute from './PrivateRoute';
-import ImageGallery from '../dashboard/ImageGallery';
 
 const Routes = () => {
   return (
@@ -17,8 +15,6 @@ const Routes = () => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/gallery" component={ImageGallery} />
         <Route exact path="/events" component={Events} />
         <Route exact path="/events/:id" component={Event} />
         <Route component={NotFound} />
